209d52c3096f5ae53b8677f0e059ad9aa47d2867
tuna <tunacinsoy@gmail.com>
2024-11-23 20:59:50 +0100

    feat: controlFlow

---
+++ ch01-intro/02-controlFlow.py
@@ -0,0 +1,8 @@
+x = 0
+
+if x > 0:
+    print(f"X is positive, since its value is {x}")
+elif x < 0:
+    print(f"X is negative, since its value is {x}")
+else:
+    print(f"X is neither negative nor positive, since its value is {x}")
