141f752011a0c35f0bb5bf35a4e3f23756f5a8ad
tuna <tunacinsoy@gmail.com>
2024-11-23 20:51:44 +0100

    feat: init

---
+++ ch01-intro/01-variablesDataTypes.py
@@ -0,0 +1,31 @@
+# Python is dynamically-typed language, meaning that we do not need to indicate
+# the variable types explicitly. Python interpreter declares the type of a variable according
+# to the value assigned to that.
+
+# DATA TYPES
+
+# Integer
+x = 5
+
+# Float
+y = 5.0
+
+# String
+z = "Hello, World!"
+
+# Boolean
+is_active = True
+
+# DATA STRUCTURES
+
+# List
+list1 = [1, 2, 3, 4, 5]
+
+# Tuple
+tuple1 = (1, 2, 3, 4, 5)
+
+# Dictionary
+person = {"name": "Tuna", "surname": "Cinsoy", "age": 99}
+
+# Set
+set1 = {1, 2, 3, 4, 5}
