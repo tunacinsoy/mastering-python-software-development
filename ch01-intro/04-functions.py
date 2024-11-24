7772492475c5632a6bb3dc2cbdf13523483d549d
tuna <tunacinsoy@gmail.com>
2024-11-24 19:52:12 +0100

    feat: args and kwargs in functions

--- ch01-intro/04-functions.py
+++ ch01-intro/04-functions.py
@@ -15,6 +15,41 @@ def calculate_age(birthYear: int) -> int:
 
 
 # TODO: Write a functions that accepts *args and **kwargs
+def differentiate_input_types(age, *args, **kwargs):
+
+    # Process age
+    if age > 40:
+        print(f"time to go home, your age is {age}.")
+
+    # Process *args
+    sum = 0
+    for arg in args:
+        if type(arg) is int:
+            sum += arg
+        elif type(arg) is float:
+            sum += arg
+        else:
+            print(arg)
+
+    print(f"sum is {sum}.")
+
+    # Process **kwargs
+    # Iterate over keys only
+    for key in kwargs.keys():
+        print(f"current provided kwarg key is: {key}")
+
+    # Iterate over values only
+    for val in kwargs.values():
+        print(f"current provided kwarg value is: {val}")
+
+    # Iterate over keys and values at the same time
+    for key, val in kwargs.items():
+        print(key, val)
+
+
+differentiate_input_types(
+    50, 2.0, 3.5, 4, 4, 5, 5, 6, "ahmet", "hulusi", key=15, tornado=23
+)
 greetMessage = greet("tuna")
 print(greetMessage)
 
